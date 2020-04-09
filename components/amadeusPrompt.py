d5ca7754cd34169d52491c977e2f1c8f7738428e
Tawmy <kersting.tom@gmail.com>
2020-04-09 14:11:38 +0200

    Add custom cancel string support to amadeusPrompt

--- components/amadeusPrompt.py
+++ components/amadeusPrompt.py
@@ -7,6 +7,7 @@ class AmadeusPrompt:
         self.bot = bot
         self.__is_user_specific = False
         self.__specified_user = None
+        self.__cancel_string = "cancel"
 
         self.__embed = discord.Embed()
         self.__embed.title = title
@@ -21,6 +22,9 @@ class AmadeusPrompt:
         self.__is_user_specific = is_user_specific
         if user is not None:
             self.__specified_user = user
+
+    async def set_cancel_string(self, cancel_string):
+        self.__cancel_string = cancel_string
 
     async def show_prompt(self, ctx, timeout_seconds, message=None):
         await self.__prepare_footer(ctx)
@@ -39,6 +43,7 @@ class AmadeusPrompt:
             else:
                 name = ctx.author.display_name
                 avatar = ctx.author.avatar_url_as(static_format="png")
+            name += " | Type \"" + self.__cancel_string + "\" to cancel"
             self.__embed.set_footer(text=name, icon_url=avatar)
 
     async def __await_user_input(self, ctx, timeout_seconds):
@@ -62,4 +67,7 @@ class AmadeusPrompt:
             return None
         else:
             await user_message.delete()
-            return user_message.content
+            if user_message.content in [self.__cancel_string, "\"" + self.__cancel_string + "\""]:
+                return None
+            else:
+                return user_message.content
