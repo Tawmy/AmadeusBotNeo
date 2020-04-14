365a593d34131ef53312809834efcc7c1fb45b24
Tawmy <kersting.tom@gmail.com>
2020-04-14 14:28:21 +0200

    Add config methods to strings.py

--- components/strings.py
+++ components/strings.py
@@ -20,6 +20,23 @@ class Strings:
         lang = await self.__get_language(ctx)
         return self.strings.get(category, {}).get(name, {}).get(lang)
 
+    async def get_config_strings(self, ctx, category, command_title):
+        lang = await self.__get_language(ctx)
+        config_option = ctx.bot.options.get(category, {}).get(command_title)
+        if config_option is not None:
+            name = config_option.get("name", {}).get(lang)
+            description = config_option.get("description", {}).get(lang)
+            return [name, description]
+        return None
+
+    async def extract_config_strings(self, ctx, object):
+        lang = await self.__get_language(ctx)
+        name = object["name"][lang]
+        description = object["description"][lang]
+        if name is not None and description is not None:
+            return [name, description]
+        return None
+
     async def __get_language(self, ctx):
         lang = ctx.bot.config.get(str(ctx.guild.id), {}).get("general", {}).get("language")
         if lang is not None:
