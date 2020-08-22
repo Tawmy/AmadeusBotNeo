4caf2668cd8524ea9a077182d98a9f4b9c7af622
Tawmy <mail@tawmy.dev>
2020-08-22 13:19:47 +0200

    Fix is_moderator not skipping consistently

--- amadeus.py
+++ amadeus.py
@@ -52,10 +52,10 @@ async def global_check(ctx):
         await check_bot_enabled(ctx, guild_config)
 
         moderator_skip_enabled = await general.deep_get(bot.config, str(ctx.guild.id), "general", "mods_override_limits")
-        user_is_moderator = True if await is_moderator(ctx) else False
 
-        await check_category_limits(ctx, guild_config, moderator_skip_enabled, user_is_moderator)
-        await check_command_limits(ctx, guild_config, moderator_skip_enabled, user_is_moderator)
+        if await check_moderator_skip(moderator_skip_enabled, await is_moderator(ctx)) is False:
+            await check_category_limits(ctx, guild_config)
+            await check_command_limits(ctx, guild_config)
 
         # TODO time limits
 
@@ -64,6 +64,10 @@ async def global_check(ctx):
 
 async def is_moderator(ctx: Context):
     return discord.utils.get(ctx.author.roles, id=bot.config[str(ctx.guild.id)]["essential_roles"]["mod_role"])
+
+
+async def check_moderator_skip(skip_enabled: bool, is_moderator: bool) -> bool:
+    return True if skip_enabled and is_moderator else False
 
 
 async def check_bot_enabled(ctx: Context, guild_config: dict):
@@ -77,16 +81,13 @@ async def check_bot_enabled(ctx: Context, guild_config: dict):
         raise ex.BotDisabled
 
 
-async def check_category_limits(ctx: Context, guild_config: dict, moderator_skip: bool, is_moderator: bool):
+async def check_category_limits(ctx: Context, guild_config: dict):
     guild_config_cat = await general.deep_get(guild_config, "limits", "categories")
     if guild_config_cat is None:
         return
     # Is extension enabled on server?
     if await general.deep_get(guild_config_cat, ctx.command.cog_name.lower(), "enabled") is False:
         raise ex.CategoryDisabled
-
-    if moderator_skip and is_moderator:
-        pass
     # Do not check for extension role limits if command has role limits specified
     elif not await command_has_role_limits(ctx, guild_config):
         # Does the extension have role limits?
@@ -99,31 +100,28 @@ async def check_category_limits(ctx: Context, guild_config: dict, moderator_skip
             raise ex.CategoryBlacklistedRole(result[1])
 
 
-async def check_command_limits(ctx: Context, guild_config: dict, moderator_skip: bool, is_moderator: bool):
+async def check_command_limits(ctx: Context, guild_config: dict):
     guild_config_com = await general.deep_get(guild_config, "limits", "commands")
     # Is command enabled on the server?
     if await general.deep_get(guild_config_com, ctx.command.name, "enabled") is False:
         raise ex.CommandDisabled
 
-    if moderator_skip and is_moderator:
-        pass
-    else:
-        # Does the command have channel limits?
-        wl = await general.deep_get_type(list, guild_config_com, ctx.command.name, "channels", "whitelist")
-        bl = await general.deep_get_type(list, guild_config_com, ctx.command.name, "channels", "blacklist")
-        if len(wl) > 0 and ctx.channel.id not in wl:
-            raise ex.CommandNotWhitelistedChannel
-        if ctx.channel.id in bl:
-            raise ex.CommandBlacklistedChannel
+    # Does the command have channel limits?
+    wl = await general.deep_get_type(list, guild_config_com, ctx.command.name, "channels", "whitelist")
+    bl = await general.deep_get_type(list, guild_config_com, ctx.command.name, "channels", "blacklist")
+    if len(wl) > 0 and ctx.channel.id not in wl:
+        raise ex.CommandNotWhitelistedChannel
+    if ctx.channel.id in bl:
+        raise ex.CommandBlacklistedChannel
 
-        # Does the command have role limits?
-        wl = await general.deep_get_type(list, guild_config_com, ctx.command.name, "roles", "whitelist")
-        bl = await general.deep_get_type(list, guild_config_com, ctx.command.name, "roles", "blacklist")
-        result = await check_role_limits(ctx, wl, bl)
-        if result[0] == 1:
-            raise ex.CommandNoWhitelistedRole(result[1])
-        if result[0] == 2:
-            raise ex.CommandBlacklistedRole(result[1])
+    # Does the command have role limits?
+    wl = await general.deep_get_type(list, guild_config_com, ctx.command.name, "roles", "whitelist")
+    bl = await general.deep_get_type(list, guild_config_com, ctx.command.name, "roles", "blacklist")
+    result = await check_role_limits(ctx, wl, bl)
+    if result[0] == 1:
+        raise ex.CommandNoWhitelistedRole(result[1])
+    if result[0] == 2:
+        raise ex.CommandBlacklistedRole(result[1])
 
 
 async def command_has_role_limits(ctx: Context, guild_config: dict) -> int:
