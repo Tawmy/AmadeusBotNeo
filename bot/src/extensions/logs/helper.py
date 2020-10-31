d7024cca0faf6dec661784a709a72ad3a8a8b436
Tawmy <mail@tawmy.dev>
2020-10-31 20:32:18 +0100

    Fix user upsert

--- bot/src/extensions/logs/helper.py
+++ bot/src/extensions/logs/helper.py
@@ -13,6 +13,11 @@ async def is_image(url: str) -> bool:
 
 
 async def add_user(bot: Bot, user_id: int):
-    db_entry = User()
-    db_entry.id = user_id
-    bot.db_session.add(db_entry)
+    db_object = bot.db_session.query(User).filter_by(id=user_id).first()
+    if db_object:
+        return
+    else:
+        db_entry = User()
+        db_entry.id = user_id
+        bot.db_session.add(db_entry)
+        bot.db_session.commit()
